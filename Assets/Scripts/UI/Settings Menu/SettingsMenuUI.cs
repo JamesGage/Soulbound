0c27c4d46a86adf01a92eb3f6de2e24f378364d3
James Gage <jgage.gm@gmail.com>
2021-03-14 15:02:54 -0500

    Added settings menu functionality for video and updated FMOD file to match project name

--- Assets/Scripts/UI/Settings Menu/SettingsMenuUI.cs
+++ Assets/Scripts/UI/Settings Menu/SettingsMenuUI.cs
@@ -1,9 +1,58 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
 
 namespace UI.Settings_Menu
 {
     public class SettingsMenuUI : MonoBehaviour
     {
+        [SerializeField] TMP_Dropdown _resolutionDropdown;
         
+        private Resolution[] _resolutions;
+
+        private void Start()
+        {
+            FindResolutions();
+        }
+
+        public void SetResolution(int resolutionIndex)
+        {
+            var resolution = _resolutions[resolutionIndex];
+            Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+        }
+        
+        public void SetQuality(int qualityIndex)
+        {
+            QualitySettings.SetQualityLevel(qualityIndex);
+        }
+
+        public void SetFullScreen(bool isFullscreen)
+        {
+            Screen.fullScreen = isFullscreen;
+        }
+        
+        private void FindResolutions()
+        {
+            _resolutions = Screen.resolutions;
+            _resolutionDropdown.ClearOptions();
+
+            List<string> options = new List<string>();
+            var currentResolutionIndex = 0;
+
+            for (int i = 0; i < _resolutions.Length; i++)
+            {
+                var option = _resolutions[i].width + " x " + _resolutions[i].height;
+                options.Add(option);
+
+                if (_resolutions[i].width == Screen.width && _resolutions[i].height == Screen.height)
+                {
+                    currentResolutionIndex = i;
+                }
+            }
+
+            _resolutionDropdown.AddOptions(options);
+            _resolutionDropdown.value = currentResolutionIndex;
+            _resolutionDropdown.RefreshShownValue();
+        }
     }
 }
