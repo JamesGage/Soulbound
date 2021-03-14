1476fd1f5fc35f0562d3501c28e31141c931d1cb
James Gage <jgage.gm@gmail.com>
2021-03-14 15:23:13 -0500

    Attached Audio sliders to FMOD buses

--- Assets/Scripts/UI/Settings Menu/SettingsMenuUI.cs
+++ Assets/Scripts/UI/Settings Menu/SettingsMenuUI.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
+using FMOD.Studio;
 
 namespace UI.Settings_Menu
 {
     public class SettingsMenuUI : MonoBehaviour
     {
+        [SerializeField] Slider masterSlider;
+        [SerializeField] Slider musicSlider;
+        [SerializeField] Slider sfxSlider;
         [SerializeField] TMP_Dropdown _resolutionDropdown;
-        
+
+        private Bus masterBus;
+        private Bus musicBus;
+        private Bus sfxBus;
         private Resolution[] _resolutions;
 
         private void Start()
         {
             FindResolutions();
+            FindBus();
         }
 
         public void SetResolution(int resolutionIndex)
@@ -29,6 +38,21 @@ namespace UI.Settings_Menu
         public void SetFullScreen(bool isFullscreen)
         {
             Screen.fullScreen = isFullscreen;
+        }
+
+        public void SetMasterVolume(float volume)
+        {
+            masterBus.setVolume(DecibelToLinear(volume));
+        }
+        
+        public void SetMusicVolume(float volume)
+        {
+            musicBus.setVolume(DecibelToLinear(volume));
+        }
+        
+        public void SetSFXVolume(float volume)
+        {
+            sfxBus.setVolume(DecibelToLinear(volume));
         }
         
         private void FindResolutions()
@@ -53,6 +77,18 @@ namespace UI.Settings_Menu
             _resolutionDropdown.AddOptions(options);
             _resolutionDropdown.value = currentResolutionIndex;
             _resolutionDropdown.RefreshShownValue();
+        }
+
+        private void FindBus()
+        {
+            masterBus = FMODUnity.RuntimeManager.GetBus("bus:/Master");
+            musicBus = FMODUnity.RuntimeManager.GetBus("bus:/Master/Music");
+            sfxBus = FMODUnity.RuntimeManager.GetBus("bus:/Master/SFX");
+        }
+
+        private float DecibelToLinear(float dB)
+        {
+           return Mathf.Pow(10.0f, dB / 20f);
         }
     }
 }
