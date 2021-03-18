dcebbedb61196ce282906acde7ab076311bbb5c6
James Gage <jgage.gm@gmail.com>
2021-03-17 21:44:29 -0500

    Added animations and stalls with some filler dialogue for a shop vender.

---
+++ Assets/Scripts/UI/PauseTime.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.UI
+{
+    public class PauseTime : MonoBehaviour
+    {
+        private void OnEnable()
+        {
+            if (SceneManager.GetActiveScene().buildIndex == 0)
+            {
+                Time.timeScale = 1f;
+                return;
+            }
+            
+            Time.timeScale = 0f;
+        }
+
+        private void OnDisable()
+        {
+            Time.timeScale = 1f;
+        }
+    }
+}
