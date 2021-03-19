6db65bec8bcf5c0648829feeb9ca1e269a4aa581
James Gage <jgage.gm@gmail.com>
2021-03-18 21:35:26 -0500

    Player now auto-attacks when being attacked by enemies

---
+++ Assets/Scripts/Combat/AutoAttack.cs
@@ -0,0 +1,39 @@
+﻿using RPG.Attributes;
+using RPG.Control;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    [RequireComponent(typeof(Fighter), typeof(Health))]
+    public class AutoAttack : MonoBehaviour
+    {
+        private Fighter _fighter;
+        private Health _health;
+        
+        private void Awake()
+        {
+            _fighter = GetComponent<Fighter>();
+            _health = GetComponent<Health>();
+        }
+
+        private void OnEnable()
+        {
+            _health.onHealthChanged += FindTarget;
+        }
+
+        private void FindTarget()
+        {
+            RaycastHit[] hits = Physics.SphereCastAll(transform.position, _fighter.GetCurrentWeapon().Range(), Vector3.up, 0);
+            foreach (var hit in hits)
+            {
+                var enemy = hit.transform.GetComponent<AIController>();
+                if(enemy == null) continue;
+
+                if(!_fighter.CanAttack(enemy.gameObject)) continue;
+                
+                _fighter.Attack(enemy.gameObject);
+                return;
+            }
+        }
+    }
+}
