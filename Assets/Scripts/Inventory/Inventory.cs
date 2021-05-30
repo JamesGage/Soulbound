f6bd35e9d3ccbb5c6fd51c948e987953bcac9d87
James Gage <jgage.gm@gmail.com>
2021-05-29 20:19:19 -0500

    Shopping items count stacking items and current inventory to know how much a player can buy

--- Assets/Scripts/Inventory/Inventory.cs
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -55,13 +55,36 @@ namespace RPG.Inventories
 
         public bool HasSpaceFor(IEnumerable<InventoryItem> items)
         {
-            var count = 0;
+            var freeSlots = FreeSlots();
+            List<InventoryItem> stackedItems = new List<InventoryItem>();
             foreach (var item in items)
             {
-                count++;
+                if (item.IsStackable())
+                {
+                    if(HasItem(item)) continue;
+                    if(stackedItems.Contains(item)) continue;
+                    stackedItems.Add(item);
+                }
+                
+                if (freeSlots <= 0) return false;
+                freeSlots--;
             }
 
-            return count <= inventorySize;
+            return true;
+        }
+
+        public int FreeSlots()
+        {
+            var count = 0;
+            foreach (InventorySlot slot in slots)
+            {
+                if (slot.number == 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
         }
 
         /// <summary>
