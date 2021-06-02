1c47fb69a093268ea0dfe27e5819399dc960d89c
James Gage <jgage.gm@gmail.com>
2021-06-02 09:23:16 -0500

    Bond regeneration added

--- Assets/Scripts/Stats/Bond.cs
+++ Assets/Scripts/Stats/Bond.cs
@@ -6,6 +6,9 @@ namespace RPG.Stats
     public class Bond : MonoBehaviour
     {
         [SerializeField] private float maxBond;
+        [Range(0f, 2f)]
+        [SerializeField] private float bondRegenerationRate = 1f;
+        [SerializeField] private float bondRegenerationAmount = 2f;
 
         private float _bond;
         public Action OnBondChanged;
@@ -13,6 +16,20 @@ namespace RPG.Stats
         private void Awake()
         {
             _bond = maxBond;
+        }
+
+        private void Update()
+        {
+            if (_bond < maxBond)
+            {
+                _bond += bondRegenerationAmount * Time.deltaTime * bondRegenerationRate;
+                OnBondChanged?.Invoke();
+                if (_bond > maxBond)
+                {
+                    _bond = maxBond;
+                    OnBondChanged?.Invoke();
+                }
+            }
         }
 
         public float GetBond()
