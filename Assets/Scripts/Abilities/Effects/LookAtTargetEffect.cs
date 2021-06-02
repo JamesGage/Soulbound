b94292d908bb79f17efe4553a13724f604ddbdde
James Gage <jgage.gm@gmail.com>
2021-06-02 09:56:55 -0500

    Casting animation and animation trigger effect added. Look at Target effect added

---
+++ Assets/Scripts/Abilities/Effects/LookAtTargetEffect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPG.Abilities.Effects
+{
+    [CreateAssetMenu(fileName = "Effect Look At Target", menuName = "Abilities/Effects/Look At Target")]
+    public class LookAtTargetEffect : EffectStrategy
+    {
+        public override void StartEffect(AbilityData data, Action finished)
+        {
+            data.GetUser().transform.LookAt(data.GetTargetPoint());
+            finished();
+        }
+    }
+}
---
+++ Assets/Scripts/Abilities/Effects/TriggerAnimationEffect.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using UnityEngine;
+
+namespace RPG.Abilities.Effects
+{
+    [CreateAssetMenu(fileName = "Effect Trigger Animation", menuName = "Abilities/Effects/Trigger Animation")]
+    public class TriggerAnimationEffect : EffectStrategy
+    {
+        [SerializeField] private string animationTrigger = "";
+        
+        public override void StartEffect(AbilityData data, Action finished)
+        {
+            var animator = data.GetUser().GetComponent<Animator>();
+            animator.SetTrigger(animationTrigger);
+            finished();
+        }
+    }
+}
