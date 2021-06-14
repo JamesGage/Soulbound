8dc1905bcb94c416b244dcc4b54a5a38eac0395f
James Gage <jgage.gm@gmail.com>
2021-06-14 15:20:05 -0500

    Added pickup resources

--- Assets/Scripts/Resource System/ResourceItem.cs
+++ Assets/Scripts/Resource System/ResourceItem.cs
@@ -1,20 +1,59 @@
-﻿using UnityEngine;
+﻿using RPG.Control;
+using UnityEngine;
 
 namespace RPG.Resource_System
 {
-    public class ResourceItem : MonoBehaviour
+    [RequireComponent(typeof(SphereCollider))]
+    public class ResourceItem : MonoBehaviour, IRaycastable
     {
         [SerializeField] private ResourceType _resourceType;
         [SerializeField] private int _resourceAmount;
 
-        public ResourceType GetResourceType()
+        private bool _canPickUp;
+        private bool _clickPickup;
+        private ResourceStore _playerResourceStore;
+
+        public CursorType GetCursorType()
         {
-            return _resourceType;
+            return CursorType.Pickup;
         }
 
-        public int GetResourceAmount()
+        public bool HandleRaycast(PlayerController callingController)
         {
-            return _resourceAmount;
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (_canPickUp)
+                {
+                    _clickPickup = true;
+                    return true;
+                }
+                callingController.GetMover().StartMoveAction(transform.position, 1f);
+                _playerResourceStore = callingController.GetComponent<ResourceStore>();
+                _clickPickup = true;
+            }
+            return true;
+        }
+        
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _canPickUp = true;
+                if (_clickPickup)
+                {
+                    _playerResourceStore.AddResource(_resourceType, _resourceAmount);
+                    _clickPickup = false;
+                    Destroy(gameObject);
+                }
+            }
+        }
+        
+        private void OnTriggerExit(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                _canPickUp = false;
+            }
         }
     }
 }
