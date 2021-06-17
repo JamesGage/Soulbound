a45df17b8e12f97f81ce61378b3f5bcd84dae763
James Gage <jgage.gm@gmail.com>
2021-06-17 16:32:12 -0500

    Created the base for the tag system

---
+++ Assets/Scripts/Tag System/TagStore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using RPG.Saving;
+using UnityEngine;
+
+namespace Tag_System
+{
+    public class TagStore : MonoBehaviour, ISaveable
+    {
+        [SerializeField] private Tags tags;
+        
+        private Dictionary<string, bool> currentTags = new Dictionary<string, bool>();
+
+        private void Awake()
+        {
+            foreach (var tag in tags.GetTags())
+            {
+                if(currentTags.ContainsKey(tag)) continue;
+                
+                currentTags.Add(tag, false);
+            }
+        }
+
+        public void SetTag(string tag, bool tagState)
+        {
+            currentTags[tag] = tagState;
+        }
+
+        public object CaptureState()
+        {
+            return currentTags;
+        }
+
+        public void RestoreState(object state)
+        {
+            currentTags = (Dictionary<string, bool>) state;
+        }
+    }
+}
---
+++ Assets/Scripts/Tag System/Tags.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Tag_System
+{
+    [CreateAssetMenu(fileName = "Tags", menuName = "Tags")]
+    public class Tags : ScriptableObject
+    {
+        [SerializeField] private string[] tags;
+
+        public string[] GetTags()
+        {
+            return tags;
+        }
+    }
+}
