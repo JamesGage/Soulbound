6f0d86a7ae0670d507c0a0aaa1e903cb5d567ae1
James Gage <jgage.gm@gmail.com>
2021-06-18 15:28:02 -0500

    Started weapon store

---
+++ Assets/Scripts/Combat/WeaponStore.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using RPG.Inventories;
+using RPG.Saving;
+using UnityEngine;
+
+namespace RPG.Combat
+{
+    public class WeaponStore : MonoBehaviour, ISaveable
+    {
+        private List<WeaponConfig> _weapons = new List<WeaponConfig>();
+        private const int _slots = 3;
+
+        public Action OnWeaponChanged;
+
+        public void AddWeapon(WeaponConfig weapon)
+        {
+            if (_weapons.Contains(weapon)) return;
+            if(!HasOpenSlot()) return;
+            
+            _weapons.Add(weapon);
+            OnWeaponChanged?.Invoke();
+        }
+
+        public void RemoveWeapon(WeaponConfig weapon)
+        {
+            if (_weapons.Contains(weapon))
+            {
+                _weapons.Remove(weapon);
+                OnWeaponChanged?.Invoke();
+            }
+        }
+        
+        public List<WeaponConfig> GetWeapons()
+        {
+            return _weapons;
+        }
+
+        private bool HasOpenSlot()
+        {
+            if (_weapons.Count == _slots) return false;
+
+            return true;
+        }
+
+        public object CaptureState()
+        {
+            var weaponConfigs = new List<string>();
+            foreach (var weapon in _weapons)
+            {
+                weaponConfigs.Add(weapon.GetItemID());
+            }
+
+            return weaponConfigs;
+        }
+
+        public void RestoreState(object state)
+        {
+            foreach (var weapon in (List<string>) state)
+            {
+                _weapons.Add(InventoryItem.GetFromID(weapon) as WeaponConfig);
+            }
+        }
+    }
+}
